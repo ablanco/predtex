aead0decd249d9d5a4546e9e2a9e1f7691b9cd4c
korosu.itai <korosu.itai@20ae2bc0-7c4c-0410-8873-ef6f59469ce9>
2008-05-12 16:23:36 +0000

    Añadido script de Perl ;)

---
+++ trunk/chopchop.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+if ($#ARGV+1 == 0) {
+  print "Usage: perl chopchop.pl arch_origen arch_destino\n(:\n";
+} else {
+  $file = $ARGV[0];
+  $dest = $ARGV[1];
+  open(IN, $file);
+  open(OUT, ">$dest");
+  while(<IN>) {
+    chomp;
+    s/[_\-;.,$()"]//g;
+    s/\n/ /g;
+    s/  / /g;
+    s/  / /g;
+    s/  / /g;
+    s/  / /g;
+    s/  / /g;
+    s/ /\n/g;
+    print OUT "$_"; print OUT "\n";
+  }
+  close(IN);
+  close(OUT);
+}
